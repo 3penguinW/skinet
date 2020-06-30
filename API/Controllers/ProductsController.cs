35708a6437f46bf83c2256fc36c0437b368d371a
Pengjun Wang <1475991262@qq.com>
2020-06-30 14:59:43 -0400

    initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+
+        }
+
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Product>>>GetProducts()/*http request state number*/
+        {
+
+           var products =  await _context.Products.ToListAsync();/*linq*/
+
+           return Ok(products);
+        }
+
+        [HttpGet("{id}")] /*end point */
+        public async Task<ActionResult<Product>> GetProduct(int id) {
+         
+         return await _context.Products.FindAsync(id);
+
+        }
+    }
+}
