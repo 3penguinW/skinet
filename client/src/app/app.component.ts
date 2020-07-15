cc1f6c10f26b066ac6f8307b295aee6eb0a63a33
Pengjun Wang <1475991262@qq.com>
2020-07-15 07:55:50 -0400

    07/15/2020

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {IProduct} from './models/product';
-import {IPagination} from './models/pagination';
-import { from } from 'rxjs';
+
+
 
 
 @Component({
@@ -13,16 +11,11 @@ import { from } from 'rxjs';
 export class AppComponent implements OnInit{
 
   title = 'SkiNet';
-  products: IProduct[];
-  constructor(private http: HttpClient){}
+
+  constructor(){}
 
   ngOnInit(): void {
-      this.http.get('https:localhost:5001/api/products?pageSize=50').subscribe(
-        (response: IPagination) => {
-        this.products = response.data;
-      }, error => {
-        console.log(error);
-      });
+
   }
 
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {CoreModule} from './core/core.module';
+import {ShopModule} from './shop/shop.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent
+    AppComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
